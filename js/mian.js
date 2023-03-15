40a66fd9cf3b9a619656b9a825cf279559ba3a18
uhhee12 <yunhee4560@gmail.com>
2023-03-15 15:45:35 +0900

    수정

--- js/mian.js
+++ js/mian.js
@@ -1,26 +1,22 @@
- $(document).ready(function () {
+$(document).ready(function () {    
 
-    if (matchMedia("screen and (min-width: 950px)").matches) {
-    // 950px 이상에서 사용할 스크립트
-        $(".menuLi").hover(function () {
+     $(".menuLi").hover(function () {
             $(".sub_menu").fadeToggle(100);
-        });
-
-    } else {
-    // 950px 미만에서 사용할 스크립트
-        $('#nav-icon4').click(function(){
+     });
+    
+    $('#nav-icon4').click(function(){
             $(this).toggleClass('open');
             $(".sub_menu").fadeToggle(100);
-        });
-            
-     }
+    });
+    
 
 
+    
      $("#nav-icon1").hover(function () {
         $(".content_menu").slideToggle();
-    });
-
+     });
      
+
     // $("#nav-icon1").click(function () {
     //     $(".content_menu").stop().slideToggle();
     //     $('#nav-icon1').toggleClass('open')
@@ -39,6 +35,7 @@
     //     window.onresize = function(){
     //     document.location.reload();
     //  };
-     
-     
- });
+    
+});
+
+
